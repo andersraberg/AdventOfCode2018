6fb705d9bf5ff040113af327a1e44c59564c43f7
Anders Råberg <anders.raberg@gmail.com>
2019-11-04 22:26:37 +0100

    Added dummy test case to run all the code.

---
+++ src/test/java/se/anders_raberg/adventofcode2018/DaysMainTest.java
@@ -0,0 +1,15 @@
+package se.anders_raberg.adventofcode2018;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class DaysMainTest {
+
+    @Test
+    public void testMain() throws IOException {
+        String[] args = { "" };
+        DaysMain.main(args);
+    }
+
+}
